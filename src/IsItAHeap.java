a1703a531b583bccbd31a389acf3d9216b8d987b
Lars Jørgensen <lars.b.jn@gmail.com>
2019-06-19 20:54:27 +0200

    Initial
    * Initial

---
+++ src/IsItAHeap.java
@@ -0,0 +1,42 @@
+public class IsItAHeap {
+
+    public static void main(String[] args) {
+        int[] A = {19,100,36,17,3,25,1,2,7};
+        boolean checkForMax = true;
+
+        int index = 0;
+        if(checkForMax) {
+            if(checkMaxHeap(A, index)){
+                System.out.println("given array is a max heap");
+            }else{
+                System.out.println("given array is not a max heap ");
+            }
+        } else {
+            if(checkMinHeap(A, index)){
+                System.out.println("given array is a min heap");
+            }else{
+                System.out.println("given array is not a min heap ");
+            }
+        }
+    }
+
+    public static boolean checkMinHeap(int[] A, int i){
+        if(2*i + 2 >A.length){
+            return true;
+        }
+        boolean left = (A[i] <= A[2*i+1] && checkMinHeap(A, 2*i+1));
+        boolean right = (2*i + 2 == A.length) || (A[i]<= A[2*i+2] && checkMinHeap(A, 2*i+2));
+
+        return left && right;
+    }
+
+    public static boolean checkMaxHeap(int[] A, int i){
+        if(2*i + 2 >A.length){
+            return true;
+        }
+        boolean left = (A[i] >= A[2*i+1] && checkMaxHeap(A, 2*i+1));
+        boolean right = (2*i + 2 == A.length) || (A[i]>= A[2*i+2] && checkMaxHeap(A, 2*i+2));
+
+        return left && right;
+    }
+}
